39898a87ef65f1de342a4b2e86581dc65f7baccc
Carlos Ortiz <cortizmardones@gmail.com>
2022-09-30 17:47:44 -0300

    first commit

---
+++ src/main/java/com/example/demo/RestControllers/indexController.java
@@ -0,0 +1,69 @@
+package com.example.demo.RestControllers;
+
+import java.util.HashMap;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuarios")
+public class indexController {
+	
+	private HashMap<String,String> listaUsuarios = new HashMap<String,String>();
+	
+	@GetMapping("/listar")
+	public HashMap<String,String> listar() {
+		return listaUsuarios;
+	}
+	
+	@PostMapping("/agregar/{usuario}/{lenguaje}")
+	public String agregar(@PathVariable String usuario, @PathVariable String lenguaje) {
+		
+		usuario = usuario.toLowerCase().trim();
+		lenguaje = lenguaje.toLowerCase().trim();
+		
+		if(listaUsuarios.containsKey(usuario)) {
+			return "Usuario ya existe en los registros anteriores";
+		} else {
+			listaUsuarios.put(usuario, lenguaje);
+			return "Usuario "+ usuario + " agregado";
+		}
+		
+	}
+	
+	@PutMapping("/actualizar/{usuario}/{lenguaje}")
+	public String actualizar(@PathVariable String usuario, @PathVariable String lenguaje) {
+		
+		usuario = usuario.toLowerCase().trim();
+		lenguaje = lenguaje.toLowerCase().trim();
+		
+		if(listaUsuarios.containsKey(usuario)){
+			listaUsuarios.replace(usuario, lenguaje);
+			return "Usuario "+ usuario + " actualizado";
+		} else {
+			return "Usuario "+ usuario + " no existe dentro de los registros";
+		}
+
+	}
+	
+	@DeleteMapping("/eliminar/{usuario}")
+	public String agregar(@PathVariable String usuario) {
+		
+		usuario = usuario.toLowerCase().trim();
+		
+		if(listaUsuarios.containsKey(usuario)) {
+			listaUsuarios.remove(usuario);
+			return "Usuario "+ usuario + " eliminado";
+		} else {
+			return "Usuario "+ usuario + " no existe dentro de los registros";
+		}
+
+	}
+	
+	
+
+}
---
+++ src/main/java/com/example/demo/ViewControllers/HomeController.java
@@ -0,0 +1,20 @@
+package com.example.demo.ViewControllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+	
+	@GetMapping("/home")
+	//@RequestMapping(value = "/home" , method = RequestMethod.GET)
+	public String home(Model model) {
+		
+		model.addAttribute("titulo","Soy un titulo");
+		return "home";
+	}
+
+}
